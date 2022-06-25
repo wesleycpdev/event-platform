fcfd823876a9d7270698ffaff6d47f1ec34fff9f
Wesley <72578914+wesleycpdev@users.noreply.github.com>
2022-06-25 18:36:05 -0300

    Update

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import CodeMockup from '../assets/code-mockup.png';
 
 export function Subscribe() {
     const navigate = useNavigate()
@@ -82,7 +83,7 @@ export function Subscribe() {
                 </div>
             </div>
 
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={CodeMockup} className="mt-10" alt="" />
         </div> 
     )
 }
